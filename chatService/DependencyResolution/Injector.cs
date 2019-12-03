58a8a843b9ed07906103617840c19dc39c588859
kartalm <mehmetbariskartal@gmail.com>
2019-12-03 06:12:44 +0300

    Dependency Injection lib integrated

---
+++ chatService/DependencyResolution/Injector.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+
+namespace DependencyResolution
+{
+    public static class Injector
+    {
+        public static IContainer Container;
+
+        public static IContainer Configure(Func<IContainer> func)
+        {
+            Container = func();
+            return func();
+        }
+
+        public static T Resolve<T>()
+        {
+            return Container.Resolve<T>();
+        }
+    }
+}
